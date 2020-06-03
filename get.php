af1663a16f425b24c5c56093f1d4435200548cea
fnooub <61742373+fnooub@users.noreply.github.com>
2020-06-03 20:11:45 +0700

    Update get.php

--- get.php
+++ get.php
@@ -7,7 +7,7 @@ $slug = $_GET['slug'];
 $s = $_GET['s'];
 $e = $_GET['e'];
 
-$HOME = 'http://' . $_SERVER["SERVER_NAME"];
+$HOME = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
 
 for ($i = $s; $i <= $e; $i++) { 
 	$urls[] = $HOME . '/getTCV.php?link=' . $link . 'chuong-' . $i . '/&slug=' . $slug;
